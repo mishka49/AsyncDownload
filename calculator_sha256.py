6951041b975f4f8af5cf86636134dfa0f012c09f
mishka49 <61920146+mishka49@users.noreply.github.com>
2024-03-19 20:50:27 +0300

    edited according wemake-python-styleguide calculator_sha256.py

--- calculator_sha256.py
+++ calculator_sha256.py
@@ -1,23 +1,57 @@
+"""Module for calculating SHA256 hash for files and directories."""
+
 import hashlib
-import os
+from pathlib import Path
 
-class Calculator:
-    @staticmethod
-    def calculate_sha256_for_file(file_path):
-        sha256_hash = hashlib.sha256()
-        with open(file_path, "rb") as file:
-            while chunk := file.read(4096):
-                sha256_hash.update(chunk)
-        return sha256_hash.hexdigest()
-
-    @staticmethod
-    def calculate_sha256_for_directory(directory_path):
-        sha256_dict = {}
-        for root, _, files in os.walk(directory_path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                sha256_hash = Calculator.calculate_sha256_for_file(file_path)
-                sha256_dict[file] = sha256_hash
-        return sha256_dict
+CHUNK_SIZE = 4096  # Size of the chunk to read and update hash.
 
 
+def read_chunks(file_stream):
+    """Read file in chunks.
+
+    Args:
+        file_stream (file): The file stream to read from.
+
+    Yields:
+        bytes: The next chunk of the file.
+    """
+    while True:
+        chunk = file_stream.read(CHUNK_SIZE)
+        if not chunk:
+            break
+        yield chunk
+
+
+def calculate_sha256_for_file(file_path: Path) -> str:
+    """Calculate SHA256 hash for a file.
+
+    Args:
+        file_path (Path): The path of the file.
+
+    Returns:
+        str: The SHA256 hash of the file.
+    """
+    sha256_hash = hashlib.sha256()
+    with file_path.open('rb') as opened_file:
+        for chunk in read_chunks(opened_file):
+            sha256_hash.update(chunk)
+    return sha256_hash.hexdigest()
+
+
+def calculate_sha256_for_directory(directory_path: str) -> dict:
+    """Calculate SHA256 hash for all files in a directory.
+
+    Args:
+        directory_path (str): The path of the directory.
+
+    Returns:
+        dict: A dictionary where the keys are the file names
+         and the values are the SHA256 hashes.
+    """
+    directory_path = Path(directory_path)
+    sha256_dict = {}
+    for file_path in directory_path.rglob('*'):
+        if file_path.is_file():
+            sha256_hash = calculate_sha256_for_file(file_path)
+            sha256_dict[file_path.name] = sha256_hash
+    return sha256_dict
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio
 
-from calculator_sha256 import Calculator
+import calculator_sha256 as calculator
 from download import Download
 from repository import Repository
 
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     download = Download(repo.HEAD_commit_files_paths, remote_repo_url, repo.directory)
     asyncio.run(download.run())
 
-    sha256_results = Calculator.calculate_sha256_for_directory(repo.directory)
+    sha256_results = calculator.calculate_sha256_for_directory(repo.directory)
 
     for file, sha256_hash in sha256_results.items():
         print(f"{file}: {sha256_hash}")
