b3b28e983871c7a428a41aedaa41ae9a023de0f0
mishka49 <andreym3ch@gmail.com>
2024-03-20 16:17:31 +0300

    refactor download.py

--- download.py
+++ download.py
@@ -1,34 +1,59 @@
+"""Module for downloading files from remote repository."""
+
+
 import asyncio
-import urllib.request
 from urllib.parse import quote
 
+import requests
 
-class Download:
+
+class Downloader(object):
+    """Represent downloader which provides methods to download files."""
+
     def __init__(self, paths, base_url, local_dir):
+        """
+         Initialize a Downloader instance.
+
+        Args:
+            paths (list): List of file paths relative to the base_url.
+            base_url (str): Path to the remote source.
+            local_dir (str): Path to the local directory.
+        """
         self.paths = paths
         self.base_url = base_url
         self.local_dir = local_dir
         self.NUMBER_OF_STREAMS = 3
 
-    async def __download_files(self):
-        while self.paths:
-            path = quote(self.paths.pop())
-
-            try:
-                await self.download_file(path)
-            except ():
-                pass
-
     async def download_file(self, path):
-        urllib.request.urlretrieve(f"{self.base_url}/src/branch/master/{path}",
-                                   f"{self.local_dir}/{path.split('/')[-1]}")
+        """
+        Download file from the remote source.
+
+        Args:
+            path (str): Relative path of the file.
+        """
+        url = '/'.join([self.base_url, 'src', 'branch', 'master', path])
+        filename = path.split('/')[-1]
+        response = requests.get(url, timeout=5)
+
+        with open('/'.join([self.local_dir, filename]), 'wb') as rf:
+            rf.write(response.content)
 
     async def run(self):
-        tasks = []
-        for _ in range(self.NUMBER_OF_STREAMS):
-            tasks.append(asyncio.create_task(self.__download_files()))
+        """
+        Run asynchronous tasks.
 
-        for task in tasks:
-            await task
-
+        Returns:
+            list: List of running tasks.
+        """
+        tasks = [
+            asyncio.create_task(self._download_files())
+            for _ in range(self.NUMBER_OF_STREAMS)
+        ]
+        await asyncio.gather(*tasks)
         return tasks
+
+    async def _download_files(self):
+        """Download files from the paths list."""
+        while self.paths:
+            path = quote(self.paths.pop())
+            await self.download_file(path)
